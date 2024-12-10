ee7a2c79ccf18b8802ddb3cc5c19be64edf239ae
ayalacj7 <orlandoayala082@gmail.com>
2024-12-10 16:21:53 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,55 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{ 
+ float num1, num2, suma,resta ,multiplicacion, divicion;
+ 
+ cout<<"ingresa tu numero 1 para sumar " ;  
+cin >> num1;   
+ cout<<"ingresa tu numero 2 para sumar " ;  
+cin >> num2;
+
+suma = num2+num1;
+
+cout<<"la suma es: "<<suma<<endl;
+    
+    
+  cout<<"ingresa tu numero 1 para restar " ;  
+cin >> num1;   
+ cout<<"ingresa tu numero 2 para restarar " ;  
+cin >> num2;
+
+resta = num1-num2;
+
+cout<<"la resta es : "<< resta<< endl;
+       
+  cout<<"ingresa tu numero 1 para divicion " ;  
+cin >> num1;   
+ cout<<"ingresa tu numero 2 para divicion  " ;  
+cin >> num2;
+
+divicion = num1/num2;
+
+cout<<"la divicion es : "<< divicion<< endl;
+          
+  cout<<"ingresa tu numero 1 para multiplicacion " ;  
+cin >> num1;   
+ cout<<"ingresa tu numero 2 para multiplicacion  " ;  
+cin >> num2;
+
+multiplicacion = num1*num2;
+
+cout<<"la multiplicacion es : "<< multiplicacion<< endl;
+          
+    
+       
+    
+    
+    
+    
+    
+
+
+    return 0;
+}
